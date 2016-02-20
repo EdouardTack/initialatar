68000881d0a072639e49cdef5a36c28258ddc9db
Edouard <edouard@tackacoder.fr>
2016-02-20 20:44:18 +0100

    Mise en place de la police + sauvegarde personnalisé

--- index.php
+++ index.php
@@ -4,11 +4,29 @@ use Initialatar\Initialatar;
 
 include './vendor/autoload.php';
 
+class File {
+ public function metho($image, $ressource) {
+    die('test');
+ }
+}
+
 $name = "Edouard Tack";
 
 $o = new Initialatar(array(
     'name'      => $name,
-    'width'     => 200,
-    'height'    => 200,
+    'width'     => 50,
+    'height'    => 50,
+    'ellipse'   => true
 ));
-$o->create()->save('test.png');
+$o->create()->save(array(new File(), 'metho'));
+
+?>
+<!DOCTYPE>
+<html>
+<head>
+
+</head>
+<body class="initial">
+    <img src="<?php echo $o->display(); ?>" alt="">
+</body>
+</html>
--- src/Initialatar.php
+++ src/Initialatar.php
@@ -1,16 +1,17 @@
 <?php
 
-
 namespace Initialatar;
-
 
 /**
  *
- * @property array _params
- * @property array _params
- * @property array _params
+ * @property array $_params
+ * @property array $_ressource
+ * @property array $image
  */
 class Initialatar {
+
+    /** @var string */
+    public $filename;
 
     /** @var array  */
     private $_params = array();
@@ -19,22 +20,30 @@ class Initialatar {
     private $_ressource;
 
     /** @var array  */
-    private $image;
+    private $_image;
+
+    /** @var string */
+    private $_filename;
 
     /**
+     * Constructor
+     *
      * @param array $params
      */
     public function __construct(array $params = array()) {
         $default = array(
             'width'     => 100,
             'height'    => 100,
+            'ellipse'   => true
         );
 
         $this->_params = $params + $default;
     }
 
     /**
+     * Create the initialatar image
      *
+     * @return \Initialatar
      */
     public function create() {
         $this->_createImage();
@@ -43,46 +52,97 @@ class Initialatar {
     }
 
     /**
+     * Return the file name to display
      *
+     * @return string
      */
     public function display() {
-        header('Content-type: image/png');
-        echo($this->image);
+        if (!is_null($this->filename))
+            return $this->filename;
+
+        return $this->_filename;
     }
 
     /**
-     * @param $filename
-     * @return mixed
+     * Saving the initialar image
+     *
+     * @param mixed
+     *   -- string
+     *   -- array with Object and method
+     *   -- callable
+     * @return bool
      */
-    public function save(string $filename) {
-        if (file_put_contents($filename, $this->image)) {
-            return $filename;
+    public function save($mixed) {
+        if (is_string($mixed)) {
+            if (file_put_contents($mixed, $this->_image)) {
+                $this->_filename = $mixed;
+                return true;
+            }
+        }
+        else if (is_array($mixed)) {
+            list($object, $method) = $mixed;
+
+            if (is_a($object, get_class($object))) {
+                $this->filename = call_user_func_array($mixed, array($this->_image, $this->_ressource));
+            }
+            else if (class_exists($object)) {
+                $this->filename = $object::$method($this->_image, $this->_ressource);
+            }
+        }
+        else if (is_callable($mixed)) {
+            $this->filename = $mixed($this->_image, $this->_ressource);
         }
     }
 
     /**
+     * Create the initialar image
      *
+     * @return void
      */
     private function _createImage() {
         $hash = md5($this->_params['name']);
         $color = substr($hash, 0, 6);
-        $image = imagecreatetruecolor($this->_params['width'], $this->_params['height']);
+        $this->_ressource = imagecreatetruecolor($this->_params['width'], $this->_params['height']);
 
-        $bg = imagecolorallocate($image, 0, 0, 0);
-        imagecolortransparent($image, $bg);
+        $bg = imagecolorallocate($this->_ressource, 0, 0, 0);
+        imagecolortransparent($this->_ressource, $bg);
 
-        $color = imagecolorallocate($image, hexdec(substr($color,0,2)), hexdec(substr($color,2,2)), hexdec(substr($color,4,2)));
-        imagefilledellipse($image, ($this->_params['width'] / 2), ($this->_params['height'] / 2), $this->_params['width'], $this->_params['height'], $color);
+        $color = imagecolorallocate($this->_ressource, hexdec(substr($color,0,2)), hexdec(substr($color,2,2)), hexdec(substr($color,4,2)));
+        imagefilledellipse($this->_ressource, ($this->_params['width'] / 2), ($this->_params['height'] / 2), $this->_params['width'] - 1, $this->_params['height'] - 1, $color);
 
         $name = explode(' ', $this->_params['name']);
-        $textcolor = imagecolorallocate($image, 255, 255, 255);
-        imagestring($image, 5, ($this->_params['width'] / 2), ($this->_params['height'] / 2), substr($name[0], 0, 1) . substr($name[1], 0, 1), $textcolor);
+        $name = (string) substr($name[0], 0, 1) . substr($name[1], 0, 1);
+        $textcolor = imagecolorallocate($this->_ressource, 255, 255, 255);
+        list($x, $y) = $this->_setFont($name, $textcolor);
+        imagestring($this->_ressource, 5, $x, $y, $name, $textcolor);
 
         ob_start();
-        $this->_ressource = imagepng($image);
+        $this->_ressource = imagepng($this->_ressource);
         $image_data = ob_get_contents();
         ob_end_clean();
-        $this->image = $image_data;
+        $this->_image = $image_data;
+    }
+
+    /**
+     * Setting the text in image
+     *
+     * @param string $text
+     * @param $textcolor
+     * @return array
+     */
+    private function _setFont(string $text) {
+        $font = 5;
+
+        $fontWidth = imagefontwidth($font);
+        $fontHeight = imagefontheight($font);
+
+        $textWidth = $fontWidth * strlen($text);
+        $positionCenter = ceil(($this->_params['width'] - $textWidth) / 2);
+
+        $textHeight = $fontHeight;
+        $positionMiddle = ceil(($this->_params['height'] - $textHeight) / 2);
+
+        return array($positionCenter, $positionMiddle);
     }
 
 }
